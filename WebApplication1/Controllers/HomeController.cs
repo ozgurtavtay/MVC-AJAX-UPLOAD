bc45035fa918816d69582fe8d3aeecdca09dfadc
semvs <semvs@ayvansaray.edu.tr>
2019-03-07 09:53:18 +0300

    Upload Image

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult FileUpload(HttpPostedFileBase file)
+        {
+
+            if (file!=null)
+            {
+                if (Directory.Exists(Server.MapPath("~/Files")) ==false )
+                {
+                    Directory.CreateDirectory(Server.MapPath("~/Files"));
+                    file.SaveAs(Path.Combine(Server.MapPath("~/Files"),file.FileName));
+                    return Json(new { hasError = false, message = "Dosya yüklendi" });
+                }
+            }
+            return Json(new { hasError = true, message = "Dosya NullB" });
+        }
+    }
+}
